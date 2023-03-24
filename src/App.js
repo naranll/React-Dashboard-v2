b27eb63882fd50e6fe2ce5fae22d16629c01639a
Morning <morning@1-9.local>
2023-03-24 09:40:04 +0800

    created new branch

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<AddNew />} />
-        <Route path="login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
